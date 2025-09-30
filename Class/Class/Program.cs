3870dc9be2c1053d155be6c3b8704edfae347aae
Ilya <888>
2025-09-30 22:22:57 +0500

    Class HW

---
+++ Class/Class/Program.cs
@@ -0,0 +1,41 @@
+﻿class Unit
+{
+    private string name;
+    private float health;
+    private int damage;
+    private float armor;
+
+    public string Name { get { return name; } }
+    public float Health { get { return health; } }
+    public int Damage { get { return damage; } }
+    public float Armor { get { return armor; } }
+
+    public Unit() : this("Unknown Unit") 
+    { 
+    
+    }
+
+    public Unit(string unitName) 
+    { 
+        name = unitName;
+        health = 100f;
+        damage = 5;
+        armor = 0.6f;
+    }
+
+    public float HealthArmor() 
+    {
+        return health * (1f + armor);
+    }
+
+    public bool SetDamage(int damage) 
+    { 
+        health -= damage * armor;
+
+        if (health <= 0) 
+        { 
+            return true;
+        }
+        return false;
+    }
+}
