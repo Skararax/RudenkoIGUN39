be925932280bb8dcf4ffe678cd5227176e1bb082
Ilya <888>
2025-10-19 19:26:05 +0500

    OOP_HW

---
+++ GamePrototype/String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+using GamePrototype.Dungeon;
+using GamePrototype.Items.EconomicItems;
+using GamePrototype.Items.EquipItems;
+using GamePrototype.Units;
+
+public enum Difficulty
+{
+    Easy,
+    Hard
+}
+
+public interface IDurability
+{
+    int Durability { get; set; }
+    int MaxDurability { get; }
+    void TakeDurabilityDamage();
+    void Repair();
+}
+
+public interface IUnitFactory
+{
+    Unit CreatePlayer(string name);
+    Unit CreateEnemy();
+}
+
+public interface IDungeonBuilder
+{
+    DungeonRoom BuildDungeon();
+}
